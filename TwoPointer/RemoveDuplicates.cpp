413b603cc0c10cc2ecdba3a0ef3f01a5b90f7c46
Yash Pandey <ypandey7412@gmail.com>
2026-06-27 23:37:42 +0530

    Add solutions for Remove Duplicates and Reverse Words in String

---
+++ TwoPointer/RemoveDuplicates.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] == nums[i]) {
+                // Duplicate, do nothing
+            } else {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
