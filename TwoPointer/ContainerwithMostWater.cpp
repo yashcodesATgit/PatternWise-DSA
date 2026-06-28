1c2891d5d2bd4a20182970f08b10c65b48692e80
Yash Pandey <ypandey7412@gmail.com>
2026-06-28 20:50:09 +0530

    Add two-pointer solutions for Container With Most Water, Move Zeroes, and Trapping Rain Water

---
+++ TwoPointer/ContainerwithMostWater.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+         
+         int n = height.size();
+         int i = 0;
+         int j = n-1;
+         int maxArea = 0;
+
+        while(i < j) {
+            int h = min(height[i],height[j]);
+            int w = j - i;
+            int area = h * w;
+          maxArea = max(area,maxArea);
+
+         if(height[i] < height[j]) {
+            i++;
+         }   
+         else {
+            j--;
+         }
+        } 
+        return maxArea;
+    }
+};
