481f391b239f911af620d111fafc6c6f2a26a4f2
Yash Pandey <ypandey7412@gmail.com>
2026-06-29 23:40:54 +0530

    Add solutions for Longest Palindrome and Palindromic Substrings

--- TwoPointer/PalindromicSubString.cpp
+++ TwoPointer/PalindromicSubString.cpp
@@ -1,3 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int Expand(string &s, int left, int right) {
+        int Count = 0;
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            Count++;
+            left--;
+            right++;
+        }
+        return Count;
+    }
+
+    int countSubstrings(string s) {
+        int n = s.length();
+        int Count = 0;
+
+        for(int i = 0; i<n; i++) {
+           Count += Expand(s, i, i);    // odd
+           Count += Expand(s, i, i+1);  // even
+
+        }
+        return Count;
+    }
+};
